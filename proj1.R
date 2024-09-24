b8c4698fc9148102113fcea901cdcf7e312c5e24
Ryanson Jonathan <rj@Ryansons-MacBook-Air-2.local>
2024-09-24 13:20:43 +0100

    add split_funct function

--- proj1.R
+++ proj1.R
@@ -10,8 +10,39 @@
 # 2) Create the Markov chain based on data extracted
 # from the novel.
 
-# Reading the file
-setwd("put/your/local/repo/location/here") ## comment out of submitted
+# Sets the working directories for the coders.
+setwd("/Users/rj/Documents/Codes/StatProg/ulysseslm") # Ryan's path
+# Feel free to add above for your paths @Joseph @Frans
+
+# Reads the file.
 a <- scan("4300-0.txt", what = "character", skip = 73, nlines = 32858 - 73,
           fileEncoding = "UTF-8")
 a <- gsub("_(", "", a, fixed = TRUE) ## remove "_("
+
+split_funct <- function(vec) {
+  # Separates punctuations from words.
+  # Input: vector
+  # Output: vector
+
+  # Gets index of words with punctuations.
+  vec_grep <- grep("[,.;!:?]", vec, fixed = FALSE)
+
+  # Creates a shifted index to place the punctuations.
+  punct_index <- vec_grep + 1:length(vec_grep)
+
+  # Creates a new vector with length of old vector + number of punctuations.
+  new_vec <- rep("", length(vec) + length(punct_index))
+
+  # Places elements of old vector into the new vector.
+  # Extracts punctuations from words with punctuations.
+  new_vec[punct_index] <- substr(vec[vec_grep], nchar(vec[vec_grep]),
+                                 nchar(vec[vec_grep]))
+  new_vec[-punct_index] <- substr(vec, 1, length(vec))
+
+  # Removes punctuations from words with punctuations.
+  new_vec[-punct_index] <- gsub("[,.;!:?]", "", new_vec[-punct_index],
+                                fixed = FALSE)
+
+  # Returns the new vector.
+  return(new_vec)
+}
